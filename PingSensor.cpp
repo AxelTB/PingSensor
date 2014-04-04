10d75ed0a82c494cd77cb7a4249c221b7a267544
AxelTB <AxelTB@teach>
2014-04-04 19:49:29 +0200

    Almost there

--- PingSensor.cpp
+++ PingSensor.cpp
@@ -1,5 +1,7 @@
 #include "PingSensor.h"
-#define _trigPulseDuration 10
+
+#define TRIGPULSEDURATION 10
+#define TIMEOUTUS 38400
 //Interrupt variables
 volatile bool interrupt_enable=false,echo_state=false;
 volatile uint16_t wave_time=0;
@@ -10,12 +12,13 @@ volatile uint16_t wave_time=0;
   */
 PingSensor::PingSensor()
 {
-   // PingSensor::pingPointer=this; //Only one instance allowed
-    interrupt_enable=false; echo_state=false; //Reset state
+    // PingSensor::pingPointer=this; //Only one instance allowed
+    interrupt_enable=false;
+    echo_state=false; //Reset state
 }
 PingSensor::~PingSensor()
 {
-	detachInterrupt(echoPin);
+    detachInterrupt(echoPin);
 }
 
 /** @brief begin
@@ -26,26 +29,38 @@ PingSensor::~PingSensor()
   *         [_timeoutus: pulse length timeout (microseconds)] (default:38400 us)
   *         [_trigPulseDuration: trigger positive edge duration] (default: 10us)
   */
-void PingSensor::begin(uint8_t _trigPin,uint8_t _echoPin)
+void PingSensor::begin(uint8_t _trigPin,uint8_t _echoPin,uint16_t _timeoutus,uint8_t _trigPulseDuration)
 {
     //Save parameters
     this->trigPin=_trigPin;
     this->echoPin=_echoPin;
+    this->timeout=_timeoutus;
     this->trigPulseDuration=_trigPulseDuration;
 
+    //Reset error
+    this->error=PSE_NOERR;
     //Init pin
     pinMode(trigPin,OUTPUT); //Pin di trigger
     pinMode(echoPin,INPUT); //Pin di echo
-    attachInterrupt(echoPin,&PingSensor::ISREcho,CHANGE); //Set interrupt on echo pin at evry front
+    attachInterrupt(echoPin,&PingSensor::ISREcho,CHANGE); //Set interrupt on echo pin at every front
 }
 
-
+//Overloaded versions-----------------------------------------
+void PingSensor::begin(uint8_t _trigPin,uint8_t _echoPin,uint16_t _timeoutus)
+{
+    begin(_trigPin,_echoPin,_timeoutus,TRIGPULSEDURATION); //Init with standard TRIGGER PULSE DURATION
+}
+void PingSensor::begin(uint8_t _trigPin,uint8_t _echoPin)
+{
+    begin(_trigPin,_echoPin,TIMEOUTUS); //Init with standard timeout
+}
+//-------------------------------------------------------------
 
 /** @brief available
   *
   * Returns 1 if a new measurement is available, 0 otherwise
   */
-uint8_t PingSensor::available()
+int8_t PingSensor::available()
 {
 
     if(!interrupt_enable && echo_state) //If new value available
@@ -53,6 +68,30 @@ uint8_t PingSensor::available()
 
     return 0;
 
+}
+
+/** @brief waitAvailable
+  *
+  * Locking function. Returns whenever new data is available (Or after given waitTimeout).
+  *@param: waitTimeout Maximum function wait time. (Default 2*_timeoutus = 76800)
+  *@return: 0 if new data available
+            1 if waitTimeout occurred
+  *
+  */
+int8_t PingSensor::waitAvailable(uint16_t waitTimeout)
+{
+ uint16_t startTime=micros();
+    while(!available())
+    {
+        if(micros()-startTime>waitTimeout)
+            return 1;
+    }
+
+    return 0; //New data available
+}
+int8_t PingSensor::waitAvailable()
+{
+    return waitAvailable(2*this->timeout);
 }
 
 
@@ -78,6 +117,22 @@ uint16_t PingSensor::getDistance()
 {
     return 0.3321f*getRoundTimeMicroseconds()/2;
 }
+/** @brief getError
+  *
+  * ***If function trigger isn't called no error occur, ever***
+  * @return 0 if no error
+  *         PSE_TIMEOUT -> positive front time too high (May be out of range)
+  *         PSE_NOFRONT -> Unable to read any front (Check connection)
+  *         PSE_FASTTRIGGER -> Trigger called before communication complete (Data may be gibberish)
+  *
+  */
+int8_t PingSensor::getError()
+{
+    return this->error;
+}
+
+
+
 
 /** @brief trigger
   *
@@ -87,13 +142,20 @@ void PingSensor::trigger()
 {
     if(interrupt_enable) //If last measurement did not complete
     {
-        //Assuming timeout reached
         interrupt_enable=false; //Disable interrupt
+
+        if(wave_time==0)
+            this->error=PSE_NOFRONT; //No front Change found
+        else if(micros()-wave_time>this->timeout)
+            this->error=PSE_TIMEOUT; //Timeout in reading wave
+        else
+            this->error=PSE_FASTTRIGGER; //Too fast trigger
+
+        wave_time=0;
         echo_state=false; //Reset state
     }
-    else
-        if(available())
-            parseTime(); //Save old measurement
+    else if(available())
+        parseTime(); //Save old measurement
 
 
     //Send a positive front to the ping sensor
@@ -110,8 +172,16 @@ void PingSensor::trigger()
   */
 void PingSensor::parseTime()
 {
-    lastRoundTime=wave_time; //Save travel time
-    echo_state=false; //Reset echo state
+    if(wave_time>this->timeout)  //If timeout occurred
+        this->error=PSE_TIMEOUT;    //Signal Timeout Error
+    else
+    {
+        this->error=PSE_NOERR;      //Reset Error
+        lastRoundTime=wave_time;    //Save travel time
+    }
+
+    wave_time=0;
+    echo_state=false;   //Reset echo state
 
 }
 
--- PingSensor.h
+++ PingSensor.h
@@ -15,13 +15,19 @@ public:
     void begin(uint8_t _trigPin,uint8_t _echoPin);
     void begin(uint8_t _trigPin,uint8_t _echoPin,uint16_t _timeoutus);
     void begin(uint8_t _trigPin,uint8_t _echoPin,uint16_t _timeoutus,uint8_t _trigPulseDuration);
+
     void trigger();
+
     uint16_t getDistance();
     uint16_t getRoundTimeMicroseconds();
-    uint8_t available();
+    int8_t  getError();
+
+    int8_t available();
+    int8_t waitAvailable();
+    int8_t waitAvailable(uint16_t waitTimeout);
 
 
-
+    uint8_t error;
 protected:
 
     void parseTime();
@@ -33,5 +39,7 @@ protected:
     uint16_t timeout;           //Read timeout
 private:
 };
+
+enum{PSE_NOERR=0,PSE_TIMEOUT,PSE_NOFRONT,PSE_FASTTRIGGER};
 
 #endif // PINGSENSOR_H
